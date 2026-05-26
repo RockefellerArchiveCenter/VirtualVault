dd8621892125c7193fdbccf0082f9edf83fb4a57
Hillel Arnold <hillel.arnold@gmail.com>
2026-05-26 16:22:26 -0400

    remove logging

--- static_aid/DataExtractor_ArchivesSpace.py
+++ static_aid/DataExtractor_ArchivesSpace.py
@@ -87,13 +87,11 @@ class DataExtractor_ArchivesSpace(DataExtractor):
         """Returns results for a list of refids modified after a given date"""
         refid_value = " OR ".join(refid_list)
         if last_export:
-            logging.info(f"Fetching {len(refid_list)} refids modified since {last_export}")
             last_export_datetime = datetime.fromtimestamp(last_export)
             last_export_datestring = last_export_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
             query = json.dumps({"query": {"jsonmodel_type": "range_query", "field": "system_mtime", "from": last_export_datestring}})
             url = f'/repositories/2/search?q=refid:{refid_value}&filter={query}&fields[]=json&page=1'
         else:
-            logging.info(f"Fetching all {len(refid_list)} refids")
             url = f'/repositories/2/search?q=refid:{refid_value}&fields[]=json&page=1'
         resp = self.aspace.client.get_paged(url)
         for r in resp:
