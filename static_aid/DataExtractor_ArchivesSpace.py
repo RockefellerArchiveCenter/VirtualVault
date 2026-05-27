1c92a64fbb1a80a18f711ecc1a810fb156b5d84e
Hillel Arnold <hillel.arnold@gmail.com>
2026-05-26 21:00:23 -0400

    better logic for handling new files

--- static_aid/DataExtractor_ArchivesSpace.py
+++ static_aid/DataExtractor_ArchivesSpace.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from datetime import datetime
 from os import getenv
 from pathlib import Path
 
@@ -31,10 +32,19 @@ class DataExtractor_ArchivesSpace(DataExtractor):
 
         for dir in category_dirs:
             refids, new_refids = self.get_refids_from_files(dir, last_export)
-            logging.info(f"Found {len(refids)} refids and {len(new_refids)} new refids in {dir.stem} directory.")
+            logging.info(f"Found {len(refids)} existing refids and {len(new_refids)} new refids in {dir.stem} directory.")
 
+            # Get updated data for existing refids
             for refid_chunk in self.list_chunks(refids):
                 updated_data = self.get_updated_data(refid_chunk, last_export)
+                for obj in updated_data:
+                    archival_object_id = obj['uri'].split("/")[-1]
+                    self.save_data_file(archival_object_id, obj,
+                                        config.destinations[dir.name])
+
+            # Get all data for new refids
+            for refid_chunk in self.list_chunks(new_refids):
+                updated_data = self.get_updated_data(refid_chunk, 0)
                 for obj in updated_data:
                     archival_object_id = obj['uri'].split("/")[-1]
                     self.save_data_file(archival_object_id, obj,
@@ -72,11 +82,11 @@ class DataExtractor_ArchivesSpace(DataExtractor):
         if dir.is_dir():
             for fp in dir.iterdir():
                 if fp.is_dir() and len(fp.name) == 32:
-                    refids.append(fp.stem)
-                    # TODO this is not working like it should.
                     created_time = fp.stat().st_ctime
                     if created_time >= last_export:
                         new_refids.append(fp.stem)
+                    else:
+                        refids.append(fp.stem)
         return refids, new_refids
 
     def list_chunks(self, lst, n=150):
@@ -86,14 +96,13 @@ class DataExtractor_ArchivesSpace(DataExtractor):
     def get_updated_data(self, refid_list, last_export):
         """Returns results for a list of refids modified after a given date"""
         refid_value = " OR ".join(refid_list)
-        # TODO fetching all the data all the time to see if we can simplify things.
-        # if last_export:
-        #     last_export_datetime = datetime.fromtimestamp(last_export)
-        #     last_export_datestring = last_export_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
-        #     query = json.dumps({"query": {"jsonmodel_type": "range_query", "field": "system_mtime", "from": last_export_datestring}})
-        #     url = f'/repositories/2/search?q=refid:{refid_value}&filter={query}&fields[]=json&page=1'
-        # else:
-        url = f'/repositories/2/search?q=refid:{refid_value}&fields[]=json&page=1'
+        if last_export:
+            last_export_datetime = datetime.fromtimestamp(last_export)
+            last_export_datestring = last_export_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
+            query = json.dumps({"query": {"jsonmodel_type": "range_query", "field": "system_mtime", "from": last_export_datestring}})
+            url = f'/repositories/2/search?q=refid:{refid_value}&filter={query}&fields[]=json&page=1'
+        else:
+            url = f'/repositories/2/search?q=refid:{refid_value}&fields[]=json&page=1'
         resp = self.aspace.client.get_paged(url)
         for r in resp:
             yield json.loads(r['json'])
